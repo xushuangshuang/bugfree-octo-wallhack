73d6745505ea33898e3b8ce934b4cd78b430475a
Dev--xu <xushuangshuang1@gmail.com>
2014-07-07 15:52:49 +0800

    增加了执行测试的代码

---
+++ run-test.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+java -classpath "lib-test/xushuangshuangJunit-1.jar;target/classes;target/classes-test;lib-test/guava-15.0.jar;lib-test/javassist-3.18.2-GA.jar;lib-test/reflections-0.9.9-RC2.jar" com.unit.XuShuangShuangJunit com.baldur
+
+echo "TEST OVER"
