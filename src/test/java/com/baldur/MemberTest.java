a11dc070d5df01907d7b8af976d5a75b4bec330c
Dev--xu <xushuangshuang1@gmail.com>
2014-07-07 16:15:05 +0800

    增加了新的案例进行测试

--- src/test/java/com/baldur/MemberTest.java
+++ src/test/java/com/baldur/MemberTest.java
@@ -9,11 +9,16 @@ public class MemberTest extends XuShuangShuangJunit
 	{
 		Member member = new Member();
 		String benben = "benben";
-
 		member.setUsername(benben);
-
 		assertEquals("benben", member.getUsername());
-		
+	}
+
+	public void test_xushuangshuang_should_be_xushuangshuang()
+	{
+		Member member = new Member();
+		String xushuangshuang = "xushuangshuang";
+		member.setUsername(xushuangshuang);
+		assertEquals("xushuangshuang", member.getUsername());
 	}
 	
 }
