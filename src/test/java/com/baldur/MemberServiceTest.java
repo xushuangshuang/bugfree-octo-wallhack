d5c30cd886c75326b71cbcd7ea9737c00e2c9f5f
Dev--xu <xushuangshuang1@gmail.com>
2014-07-08 00:03:09 +0800

    增加了一个测试案例

--- src/test/java/com/baldur/MemberServiceTest.java
+++ src/test/java/com/baldur/MemberServiceTest.java
@@ -1,11 +1,20 @@
 package com.baldur;
 
 import com.unit.XuShuangShuangJunit;
+import com.baldur.google.Member;
+import com.baldur.google.MemberService;
 
 public class MemberServiceTest extends XuShuangShuangJunit
 {
-	public void test_username_is_empty_should_be_save()
+	public void test_benben_is_empty_should_be_save()
 	{
-			
+		Member member = new Member();
+		member.setUsername("benben");
+
+		MockMemberDao memberDao = new MockMemberDao();
+		MemberService memberService = new MemberService(memberDao);
+		memberService.save(member);
+		
+		assertTrue(memberDao.saveHasInvoked);
 	}
 }
