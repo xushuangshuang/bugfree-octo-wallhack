17612d3f057365da072d3587538eac55b19224f6
Dev--xu <xushuangshuang1@gmail.com>
2014-07-07 16:23:01 +0800

    重构了两个测试案例的代码

--- src/test/java/com/baldur/MemberTest.java
+++ src/test/java/com/baldur/MemberTest.java
@@ -5,9 +5,9 @@ import com.baldur.google.Member;
 
 public class MemberTest extends XuShuangShuangJunit
 {
+	Member member = new Member();
 	public void test_benben_should_be_benben()
 	{
-		Member member = new Member();
 		String benben = "benben";
 		member.setUsername(benben);
 		assertEquals("benben", member.getUsername());
@@ -15,7 +15,6 @@ public class MemberTest extends XuShuangShuangJunit
 
 	public void test_xushuangshuang_should_be_xushuangshuang()
 	{
-		Member member = new Member();
 		String xushuangshuang = "xushuangshuang";
 		member.setUsername(xushuangshuang);
 		assertEquals("xushuangshuang", member.getUsername());
