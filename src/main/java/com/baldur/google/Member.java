e1482437cd9eec92f1f2d868e480557c5681a944
Dev--xu <xushuangshuang1@gmail.com>
2014-07-08 07:06:00 +0800

    在Member中配置了id的属性

--- src/main/java/com/baldur/google/Member.java
+++ src/main/java/com/baldur/google/Member.java
@@ -3,7 +3,16 @@ package com.baldur.google;
 public class Member
 {
 	private String username;
+	private Long id;
 
+	public void setId(Long id)
+	{
+		this.id = id;
+	}
+	public Long getId()
+	{
+		return id;
+	}
 	public void setUsername(String username)
 	{
 		this.username = username;
