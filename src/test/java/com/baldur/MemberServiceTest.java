b472b262f16d3dfd58564f7dc78662145919ddc1
Dev--xu <xushuangshuang1@gmail.com>
2014-07-08 00:45:27 +0800

    删除了无用代码

--- src/test/java/com/baldur/MemberServiceTest.java
+++ src/test/java/com/baldur/MemberServiceTest.java
@@ -1,26 +1,20 @@
 package com.baldur;
 
-import com.unit.XuShuangShuangJunit;
-import com.baldur.google.Member;
 import com.baldur.google.MemberService;
 
-public class MemberServiceTest extends XuShuangShuangJunit
+public class MemberServiceTest extends BaldurGoogleTestCase
 {
 	MockMemberDao memberDao = new MockMemberDao();
 	MemberService memberService = new MemberService(memberDao);
 	public void test_benben_is_not_empty_should_be_save()
 	{
-		Member member = new Member();
-		member.setUsername("benben");
-		memberService.save(member);
+		memberService.save(creatMemberWithUsername("benben"));
 		assertTrue(memberDao.saveHasInvoked);
 	}
 
 	public void test_username_is_empty_should_not_save()
 	{
-		Member member = new Member();
-		member.setUsername("  ");
-		memberService.save(member);
+		memberService.save(creatMemberWithUsername("  "));
 		assertFalse(memberDao.saveHasInvoked);
 	}
 }
