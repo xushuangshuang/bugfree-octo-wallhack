7b50296406f4c4c95924ba4f8ee7536bb57ba1ce
Dev--xu <xushuangshuang1@gmail.com>
2014-07-08 07:17:45 +0800

    增加了对id属性的测试

--- src/test/java/com/baldur/MemberTest.java
+++ src/test/java/com/baldur/MemberTest.java
@@ -1,17 +1,25 @@
 package com.baldur;
 
+import java.util.Date;
+
 public class MemberTest extends BaldurGoogleTestCase
 {
-	public void test_benben_should_be_benben()
+	public void test_benben_should_be_set()
 	{
 		String benben = "benben";
 		assertEquals("benben", creatMemberWithUsername(benben).getUsername());
 	}
 
-	public void test_xushuangshuang_should_be_xushuangshuang()
+	public void test_xushuangshuang_should_be_set()
 	{
 		String xushuangshuang = "xushuangshuang";
 		assertEquals("xushuangshuang",creatMemberWithUsername(xushuangshuang).getUsername());
 	}
 	
+	public void test_id_should_be_set()
+	{
+		Long id = new Date().getTime();
+		assertEquals(id, creatMemberById(id).getId());
+	
+	}
 }
