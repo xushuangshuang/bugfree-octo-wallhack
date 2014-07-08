65ee0b0f40dbf262212f91fd50a8915e58db559d
Dev--xu <xushuangshuang1@gmail.com>
2014-07-08 09:40:34 +0800

    虚拟实现了update的实现

--- src/test/java/com/baldur/MockMemberDao.java
+++ src/test/java/com/baldur/MockMemberDao.java
@@ -6,9 +6,19 @@ import com.baldur.google.Member;
 public class MockMemberDao implements MemberDao
 {
 	public boolean saveHasInvoked = false;
+	public boolean updateHasInvoked = false;
+	public Member expectedMember;
 	public Member save(Member member)
 	{
 		saveHasInvoked = true;
+		return member;
+	}
+
+	public Member update(Member member)
+	{
+		updateHasInvoked = true;
+		expectedMember.setUsername(member.getUsername());
+		expectedMember.setId(member.getId());
 		return member;
 	}
 }
