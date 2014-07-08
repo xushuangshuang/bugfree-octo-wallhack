420f30bf0dc78ed68f4cf1826c9fb5af7038f8f5
Dev--xu <xushuangshuang1@gmail.com>
2014-07-08 10:42:59 +0800

    修改了编译脚本中的引包

--- compile.sh
+++ compile.sh
@@ -8,6 +8,6 @@ mkdir -p target/classes-test
 
 javac -d target/classes src/main/java/com/baldur/google/*.java
 
-javac  -d target/classes-test  -classpath "target/classes;lib-test/xushuangshuangJunit-1.jar" src/test/java/com/baldur/*.java
+javac  -d target/classes-test  -classpath "target/classes;lib-test/xushuangshuangJunit-3.jar" src/test/java/com/baldur/*.java
 
 echo "COMPILE  SUCCESS"
