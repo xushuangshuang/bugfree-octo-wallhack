211e5972162e08254665b52d0449f4fa4a2489ee
Dev--xu <xushuangshuang1@gmail.com>
2014-07-08 14:26:42 +0800

    增加了新的测试案例用于判断修改的时候需要进行判断修改的时候存在

--- src/test/java/com/baldur/MemberServiceTest.java
+++ src/test/java/com/baldur/MemberServiceTest.java
@@ -24,8 +24,14 @@ public class MemberServiceTest extends BaldurGoogleTestCase
 	}
 	public void test_benben_id_1993_change_2000_should_be_change()
 	{
-		memberDao.expectedMember = creatMemberByIdAndUsername(1990L,"xushuangshuang"); 
+		memberDao.expectedMember = creatMemberByIdAndUsername(1990L,"benben"); 
 		memberService.update(creatMemberByIdAndUsername(2000L,"benben"));
 		assertEquals(2000L, memberDao.expectedMember.getId());
+	}
+	public void test_xushuangshuang_id_1890_change_2004_shoule_be_change()
+	{
+		memberDao.expectedMember = creatMemberByIdAndUsername(2005L,"xushuangshuang");
+		memberService.update(creatMemberByIdAndUsername(1980L,"benben"));
+		assertFalse(memberDao.updateHasInvoked);
 	}
 }
