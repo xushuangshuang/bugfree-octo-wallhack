989c5868191f5ac91d90ad753f0902b8607ce3ad
Dev--xu <xushuangshuang1@gmail.com>
2014-07-08 14:24:45 +0800

    将新的Junit添加进脚本中

--- run-test.sh
+++ run-test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-java -classpath "lib-test/xushuangshuangJunit-3.jar;target/classes;target/classes-test;lib-test/guava-15.0.jar;lib-test/javassist-3.18.2-GA.jar;lib-test/reflections-0.9.9-RC2.jar" com.unit.XuShuangShuangJunit com.baldur
+java -classpath "lib-test/xushuangshuangJunit-4.jar;target/classes;target/classes-test;lib-test/guava-15.0.jar;lib-test/javassist-3.18.2-GA.jar;lib-test/reflections-0.9.9-RC2.jar" com.unit.XuShuangShuangJunit com.baldur
 
 echo "TEST OVER"
