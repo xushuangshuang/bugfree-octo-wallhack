b771d3970d151404f4e9468445dbea7654332a0a
Dev--xu <xushuangshuang1@gmail.com>
2014-07-10 15:08:11 +0800

    修改了测试案例的方法名

--- src/test/java/com/baldur/MemberServiceTest.java
+++ src/test/java/com/baldur/MemberServiceTest.java
@@ -22,13 +22,13 @@ public class MemberServiceTest extends BaldurGoogleTestCase
 		memberService.save(creatMemberWithUsername(null));
 		assertFalse(memberDao.saveHasInvoked);
 	}
-	public void test_benben_id_1993_change_2000_should_be_change()
+	public void valid_member_should_be_changed()
 	{
 		memberDao.expectedMember = creatMemberByIdAndUsername(1990L,"benben"); 
 		memberService.update(creatMemberByIdAndUsername(2000L,"benben"));
 		assertEquals(2000L, memberDao.expectedMember.getId());
 	}
-	public void test_xushuangshuang_id_1890_change_2004_shoule_be_change()
+	public void test_xushuangshuang_id_1890_change_2004_should_be_change()
 	{
 		memberDao.expectedMember = creatMemberByIdAndUsername(2005L,"xushuangshuang");
 		memberService.update(creatMemberByIdAndUsername(1980L,"benben"));
